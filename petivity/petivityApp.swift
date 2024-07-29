39710bfd52580d4e8e9bf34980bac7a681c40dbb
Jasmine Liu <jasmineliu331@gmail.com>
2024-07-29 15:23:43 -0700

    Initial Commit

---
+++ petivity/petivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  petivityApp.swift
+//  petivity
+//
+//  Created by Scholar on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct petivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
